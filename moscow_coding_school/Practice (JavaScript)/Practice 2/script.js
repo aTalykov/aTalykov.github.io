7098b1c2772dd39e0ff1156ef7d5f4af1a6ed2bd
aTalykov <arzamesh@gmail.com>
2020-12-07 23:10:27 +0300

    Практика 2 (исправления)

--- moscow_coding_school/Practice (JavaScript)/Practice 2/script.js
+++ moscow_coding_school/Practice (JavaScript)/Practice 2/script.js
@@ -1,8 +1,8 @@
-totalCash = prompt("Количество денег?")
-watchesCount = parseInt(prompt("Количество часов?"))
-earringsCount = prompt("Количество серёжек")
-watchesSum = watchesCount * parseInt(prompt("Стоимость часов?"))
-earringsSum = earringsCount * parseInt(prompt("Стоимость серёжек?"))
+let totalCash = parseInt(prompt("Количество денег?"));
+let watchesCount = parseInt(prompt("Количество часов?"));
+let earringsCount = parseInt(prompt("Количество серёжек"));
+let watchesSum = watchesCount * parseInt(prompt("Стоимость часов?"));
+let earringsSum = earringsCount * parseInt(prompt("Стоимость серёжек?"));
 
 // if else использован исключительно для проверки
-console.log(totalCash >= (watchesSum + earringsSum))
+console.log(totalCash >= (watchesSum + earringsSum));
