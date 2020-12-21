339fc8a6a9ff3ad32d295c3a728e80a3ef6aee86
aTalykov <arzamesh@gmail.com>
2020-12-22 00:58:36 +0300

    Практика 6.4

---
+++ moscow_coding_school/Practice (JavaScript)/Practice_6.4/js/script.js
@@ -0,0 +1,26 @@
+const APIKey = '099715979800997fed902c8c415868c1';
+const form = document.forms[0];
+const cityTemperature = document.querySelector('.cityTemperature');
+const inputtedCity = document.querySelector('.inputtedCity');
+const cityWindSpeed = document.querySelector('.cityWindSpeed');
+
+form.onsubmit = function (e) {
+    e.preventDefault();
+    const city = form.elements.city.value;
+    inputtedCity.innerHTML = city;
+
+    const url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+APIKey;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, false);
+    xhr.send();
+
+    if (xhr.status !== 200) {
+        console.log(xhr.status + ' ' + xhr.statusText)
+    } else {
+        let DATA = JSON.parse(xhr.responseText);
+        console.log(DATA);
+        cityTemperature.innerHTML = Math.round(DATA.main.temp - 273) + ' градус Цельсия';
+        cityWindSpeed.innerHTML = DATA.wind.speed + ' м/c';
+    }
+}
